9d8b67fdbeab1e0a03b7aa1c88805f9b4719d0bf
SMASobur <sikdar.se@gmail.com>
2026-02-26 12:30:13 +0100

    Add error handling for division by zero

--- src/main/java/se/lexicon/CalculatorApp.java
+++ src/main/java/se/lexicon/CalculatorApp.java
@@ -45,8 +45,12 @@ public class CalculatorApp {
                     break;
 
                 case '/':
-                    result = num1 / num2;
-                    System.out.println("Result: " + result);
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                        System.out.println("Result: " + result);
+                    } else {
+                        System.out.println("Error: Division by zero.");
+                    }
                     break;
 
                 default:
